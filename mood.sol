fd348f791a66508f0623df20567f1397599fea98
shreyas0924 <shreyasp0924@gmail.com>
2022-10-16 21:29:34 +0530

    first dapp

--- mood.sol
+++ mood.sol
@@ -6,10 +6,7 @@ contract MoodDiary {
     function setMood(string memory _mood) public{
         mood = _mood;
     }
-
     function getMood() public view returns(string memory){
         return mood;
     }
-
-
 }
